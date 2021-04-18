a88eee305e341bb218333f94fcdb5506ff56036f
Loveneesh Singh <lovenes@thoughtworks.com>
2021-04-18 16:55:50 +0800

    Add player age with player name in the game

--- src/com/example/java/game/GameApplication.java
+++ src/com/example/java/game/GameApplication.java
@@ -1,9 +1,6 @@
 package com.example.java.game;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class GameApplication {
     private static final int randomNumber = (new Random()).nextInt(100);
@@ -12,21 +9,26 @@ public class GameApplication {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of players:");
         int numberOfPlayers = scanner.nextInt();
-        List<String> listOfPlayers = new ArrayList<>();
+        List<Map<String, String>> listOfPlayers = new ArrayList<>();
 
         for (int playerIndex = 1; playerIndex <= numberOfPlayers; playerIndex++) {
-            System.out.println("Add player " + playerIndex + " name:");
-            listOfPlayers.add(scanner.next());
+            System.out.println("Add player " + playerIndex + " name and age:");
+            Map<String, String> player = new HashMap<>();
+            System.out.println("Name: ");
+            player.put("name", scanner.next());
+            System.out.println("Age: ");
+            player.put("age", scanner.next());
+            listOfPlayers.add(player);
         }
 
         int playerTurn = 0;
         while (true) {
-            String playerName = listOfPlayers.get(playerTurn);
-            System.out.println("Player " + playerName + " turn:");
+            Map<String, String> player = listOfPlayers.get(playerTurn);
+            System.out.println("Player " + player.get("name") + " turn:");
             System.out.println("Guess a number between 1 to 100");
             int guessedNumber = scanner.nextInt();
             if (guessedNumber == randomNumber) {
-                System.out.println("Congratulation " + playerName + " you won the game");
+                System.out.println("Congratulation " + player.get("name") + " " + player.get("age") + " you won the game");
                 break;
             } else if (guessedNumber < randomNumber) {
                 System.out.println("Guessed number is less than the random number");
